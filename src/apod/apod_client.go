8bbf8f2da3c71a396bdae8712fc1e77646328ac3
Kevin <kevinkwon1995@gmail.com>
2020-03-25 21:26:08 -0700

    Allow CORS

--- src/apod/apod_client.go
+++ src/apod/apod_client.go
@@ -24,13 +24,19 @@ func init() {
 
 // GetBatchImages will retrieve multiple images from NASA APOD API
 func GetBatchImages(w http.ResponseWriter, r *http.Request) {
-	log.Print(r.URL.Query())
+	//Allow CORS here By * or specific origin
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	count, convErr := strconv.Atoi(r.URL.Query().Get("count"))
 
 	if convErr != nil {
 		log.Fatal(convErr)
 	}
 
+	// Get today's date in UTC (That is how APOD operates)
+	// convert to specified ISO, and find start date to call API and retrieve
+	// the correct number of images
 	today := time.Now()
 	endDate := today.Format(layoutISO)
 	startDate := today.AddDate(0, 0, (count-1)*-1).Format(layoutISO)
