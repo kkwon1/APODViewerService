83446080cb83c52d24d28747dc4158fe4544c193
Kevin Kwon <kevinkwon1995@gmail.com>
2020-05-14 11:00:07 -0700

    Moved firebase secrets to an environment variable

--- utils/verify_token.go
+++ utils/verify_token.go
@@ -2,14 +2,11 @@ package utils
 
 import (
 	"fmt"
-	"os"
 
 	"golang.org/x/net/context"
 
 	firebase "firebase.google.com/go"
 	log "github.com/sirupsen/logrus"
-
-	"google.golang.org/api/option"
 )
 
 type TokenVerifier interface {
@@ -24,24 +21,18 @@ func NewTokenVerifier() TokenVerifier {
 }
 
 func (tv *tokenVerifier) VerifyToken(ctx context.Context, idToken string) (bool, error) {
-	credentials_file := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
-	opt := option.WithCredentialsFile(credentials_file)
-	app, err := firebase.NewApp(context.Background(), nil, opt)
+	app, err := firebase.NewApp(context.Background(), nil)
 	if err != nil {
-		log.Errorf("error initializing app: %v", err)
 		return false, fmt.Errorf("error initializing app: %v", err)
 	}
 
 	client, auth_err := app.Auth(ctx)
 	if auth_err != nil {
-		log.Errorf("error getting Auth client: %v\n", auth_err)
 		return false, fmt.Errorf("error getting Auth client: %v\n", auth_err)
 	}
 
 	token, verify_err := client.VerifyIDToken(ctx, idToken)
 	if verify_err != nil {
-		log.Errorf("error verifying ID token: %v\n", verify_err)
 		return false, fmt.Errorf("error verifying ID token: %v\n", verify_err)
 	}
 
